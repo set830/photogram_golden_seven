a830f6dcb3514a294ffa03c3469b9de62d95cfe0
set830 <SET@Shawns-iMac.local>
2015-10-30 05:55:22 -0500

    photo details work

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,15 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
+    render ("index.html.erb")
+  end
+
+  def show
+    p = Photo.find_by({ :id => params[:id] })
+    @photo_source = p.source
+    @photo_caption = p.caption
+    @photo_id = p.id
+    render ("show.html.erb")
   end
 
 
